8f2e078b5693384b5c56d8ba9780d761a6e20fdf
Christian KULA <christian.kula.ck@gmail.com>
2017-11-02 14:36:00 +0100

    Rename MainActivity to BoatsFeedActivity

---
+++ app/src/main/java/com/christiankula/boatstagram/feed/BoatsFeedActivity.java
@@ -0,0 +1,15 @@
+package com.christiankula.boatstagram.feed;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.christiankula.boatstagram.R;
+
+public class BoatsFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boats_feed);
+    }
+}
